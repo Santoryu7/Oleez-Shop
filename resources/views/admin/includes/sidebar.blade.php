fa0d28c7fa5218b303420e84eef0f8aa1250f8f0
Santoryu7 <keyongurren@gmail.com>
2023-05-04 22:12:25 +0300

    addCreateCategories

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -6,7 +6,7 @@
 
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('admin.category.index') }}" class="nav-link">
                     <p>
                         Категории
                     </p>
