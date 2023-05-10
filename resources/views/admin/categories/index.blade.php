733bbd8e5c42d9f12095b55a06647854b19e6fda
Santoryu7 <keyongurren@gmail.com>
2023-05-10 21:57:31 +0300

    editCreateCategories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Категории</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -24,17 +24,41 @@
         <section class="content">
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
-                <div class="row">
+                <div class="row mb-3">
                     <div class="col-1">
                         <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
                     </div>
-                    <div class="col-12">
-                        Категории
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название категории</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{ $category->id }}</td>
+                                            <td>{{ $category->title }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
                     </div>
                     <!-- ./col -->
                 </div>
+            </div>
                 <!-- /.row -->
-            </div><!-- /.container-fluid -->
         </section>
         <!-- /.content -->
     </div>
