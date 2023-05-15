eeeb2263a55b90b720313b11a91dd705cf2822ba
Santoryu7 <keyongurren@gmail.com>
2023-05-15 17:13:13 +0300

    addDeleteProducts

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -39,7 +39,7 @@
                                     <tr>
                                         <th>ID</th>
                                         <th>Название товара</th>
-                                        <th colspan="2">Действие</th>
+                                        <th colspan="3" class="text-center">Действие</th>
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -47,8 +47,17 @@
                                         <tr>
                                             <td>{{ $product->id }}</td>
                                             <td>{{ $product->title }}</td>
-                                            <td><a href="{{ route('admin.product.show', $product->id) }}"><img width="20" height="20" src="{{ asset('img/eye-solid.svg') }}" alt=""></a></td>
-                                            <td><a href="{{ route('admin.product.edit', $product->id) }}"><img width="20" height="20" src="{{ asset('img/pencil-solid.svg') }}" alt=""></a></td>
+                                            <td class="text-center"><a href="{{ route('admin.product.show', $product->id) }}"><img width="20" height="20" src="{{ asset('img/eye-solid.svg') }}" alt=""></a></td>
+                                            <td class="text-center"><a href="{{ route('admin.product.edit', $product->id) }}"><img width="20" height="20" src="{{ asset('img/pencil-solid.svg') }}" alt=""></a></td>
+                                            <td class="text-center">
+                                                <form action="{{ route('admin.product.delete', $product->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="border-0 bg-transparent">
+                                                        <img width="20" height="20" src="{{ asset('img/trash-solid.svg') }}" alt="">
+                                                    </button>
+                                                </form>
+                                            </td>
                                         </tr>
                                     @endforeach
                                     </tbody>
