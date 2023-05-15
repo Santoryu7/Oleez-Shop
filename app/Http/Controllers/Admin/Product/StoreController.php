8f5f7eef84134c7982874bbd5c177da2db9ead3f
Santoryu7 <keyongurren@gmail.com>
2023-05-15 19:25:17 +0300

    addAnImageToAProduct

--- app/Http/Controllers/Admin/Product/StoreController.php
+++ app/Http/Controllers/Admin/Product/StoreController.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Product\StoreRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        Product::firstOrCreate($data);
+        $data['image'] = Storage::put('/images', $data['image']);;
 
+        Product::firstOrCreate($data);
         return redirect()->route('admin.product.index');
     }
 }
