849b14c8d074eb6e8f68db0dc789ac927646acba
Santoryu7 <keyongurren@gmail.com>
2023-05-19 21:41:36 +0300

    addCRUDForUsers

---
+++ resources/views/admin/user/show.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layouts.main')
+@section('content')
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6 d-flex align-items-center">
+                        <h1 class="m-0 mr-2">{{ $user->name }}</h1>
+                        <a href="{{ route('admin.user.edit', $user->id) }}"><img width="20" height="20" src="{{ asset('img/pencil-solid.svg') }}" alt=""></a>
+                        <form action="{{ route('admin.user.delete', $user->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="border-0 bg-transparent">
+                                <img width="20" height="20" src="{{ asset('img/trash-solid.svg') }}" alt="">
+                            </button>
+                        </form>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row mb-3">
+                    <div class="col-6">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <tbody>
+                                    <tr>
+                                        <td>Имя</td>
+                                        <td>Id</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->id }}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                        <!-- ./col -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
