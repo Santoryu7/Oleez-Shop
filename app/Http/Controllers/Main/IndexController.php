3773e78a2a927b313506655248ad46c4c9f5079d
Santoryu7 <keyongurren@gmail.com>
2023-05-26 17:32:22 +0300

    editHomePage

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index');
+        $products = Product::paginate(6);
+        return view('main.index', compact('products'));
     }
 
 
